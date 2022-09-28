64e53161d03c3a70a32d3b3545a75e129c0e2de7
Dawnmyth <137096871@qq.com>
2022-09-28 19:39:33 +0800

    home

--- main.js
+++ main.js
@@ -3,6 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入uniapp网络请求的第三方包
+import {$http} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 定义根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 全局定义请求失败的弹框方法
+uni.$showMsg = function (title = '请求失败！', duration = 1500){
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })} 
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title:"加载中"
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function(options) {
+  uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
